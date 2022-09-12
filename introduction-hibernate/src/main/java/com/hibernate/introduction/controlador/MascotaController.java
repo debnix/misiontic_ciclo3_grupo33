1dbecc4726fc4607ec09136282d76531fdbec1f3
debnix <daquipe5@gmail.com>
2022-09-12 08:12:44 -0500

    introduction-hibernate: add getXRaza() into MascotaController.java

--- introduction-hibernate/src/main/java/com/hibernate/introduction/controlador/MascotaController.java
+++ introduction-hibernate/src/main/java/com/hibernate/introduction/controlador/MascotaController.java
@@ -1,5 +1,8 @@
 package com.hibernate.introduction.controlador;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -41,6 +44,20 @@ public class MascotaController {
     }
     session.close();
     return resp;
+  }
+
+  public List<Mascota> getXRaza(String raza) {
+    List<Mascota> mascotas = new ArrayList<>();
+    Session session = openSession();
+    try {
+      mascotas = session.createQuery("from Mascota where raza = :param_raza", Mascota.class)
+          .setParameter("param_raza", raza)
+          .list();
+      session.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return mascotas;
   }
 
 }
