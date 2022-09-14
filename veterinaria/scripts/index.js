1bfcb46bd663cf724fb8db4b8ef463b63fc05b04
debnix <daquipe5@gmail.com>
2022-09-14 09:13:10 -0500

    veterinaria: add functions -> listar_mascotas and get_mascotas

--- veterinaria/scripts/index.js
+++ veterinaria/scripts/index.js
@@ -6,10 +6,32 @@ async function get_mascotas (url) {
   return mascotas
 }
 
+function listar_mascotas (mascotas) {
+  // Referenciar tabla
+  const table = document.getElementById("table")
+  let tbody = "<tbody>"
+  // Iterar mascotas
+  for (let i = 0; i < mascotas.length; i++) {
+    const m = mascotas[i]
+    tbody += `<tr> 
+                <td> 
+                  <img src="${m.foto}"/>
+                </td>
+                <td>${m.nombre}</td>
+                <td>${m.apellido}</td>
+                <td>${m.raza}</td>
+                <td>${m.observacion}</td>
+                <td></td>
+              </tr>`
+  }
+  tbody += "</tbody>"
+  table.innerHTML += tbody
+}
+
 async function main () {
   const url = "http://localhost:8080/mascotas";
   const mascotas = await get_mascotas(url)
-  console.log(mascotas)
+  listar_mascotas(mascotas)
 }
 
 main()
