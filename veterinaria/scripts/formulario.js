327d4f2321cd48a3d48a00d7b3f57976e7aa6f20
debnix <daquipe5@gmail.com>
2022-09-19 07:48:07 -0500

    veterinaria: add style into formulario.html and get data into formulario.html

--- veterinaria/scripts/formulario.js
+++ veterinaria/scripts/formulario.js
@@ -2,6 +2,13 @@
 function get_data_form (evt) {
   // Indicar que no recarge página al enviar el formulario
   evt.preventDefault()
-  let nombre = evt.target.nombre.value;
-  console.table({ nombre })
+  const form = evt.target
+  const mascota = {
+    nombre: form.nombre.value,
+    apellido: form.apellido.value,
+    raza: form.raza.value,
+    foto: form.foto.value,
+    observacion: form.observacion.value
+  }
+  console.table({ mascota })
 }
